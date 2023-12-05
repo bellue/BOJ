66efe42937b01d0fa421a1aea3630c08fb53e250
Jeong Jong Woo <90558704+on-jwo@users.noreply.github.com>
2023-12-05 14:43:27 +0000

    add

--- C++class/1753.cpp
+++ C++class/1753.cpp
@@ -15,7 +15,30 @@ vector<pair<int, int>> v[N];
 int d[N];
 
 void dijkstra(int start) {
-
+    priority_queue<pair<int,int>> pq;
+    d[start] = 0;
+    pq.push(make_pair(start,0));
+    
+    while(!pq.empty()) {
+        int nownode = pq.top().first;
+        int nowdist = -pq.top().second;
+        pq.pop();
+        
+        if(d[nownode] < nowdist) continue;
+        
+        for (int i = 0; i < v[nownode].size(); i ++) {
+            int next = v[nownode][i].first;
+            int nextdist = v[nownode][i].second + nowdist;
+        
+            if(d[next] > nextdist) {
+                d[next] = nextdist;
+                pq.push(make_pair(next,nextdist));
+            }
+        }
+        
+        
+    }
+    
 }
 
 int main() {
@@ -28,7 +51,15 @@ int main() {
         int node1, node2, dist; cin >> node1 >> node2 >> dist;
         v[node1].push_back(make_pair(node2, dist));
     }
+    for (int i = 0; i < nodenum; i ++) {
+        d[i] = INF;
+    }
     
-
-
+    dijkstra(start);
+    
+    for (int i = 0; i < nodenum; i ++) {
+        if(d[i] == INF) cout << "INF" << '\n';
+        else cout << d[i] << '\n';
+    }
+    
 }
