e541acdf5e27c4b8e4f6d831157e6416c3acda05
Jeong Jong Woo <90558704+on-jwo@users.noreply.github.com>
2023-12-14 14:27:33 +0000

    add

--- C++class/14502.cpp
+++ C++class/14502.cpp
@@ -1,9 +1,10 @@
-//https://salon.tistory.com/73
+// bfs 연구소
 
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <string.h>
 
 using namespace std;
 
@@ -16,8 +17,65 @@ int ans;
 int tmp[N][N];
 bool visited[N][N];
 
-void bfs(int start) {
+void bfs() { // 바이러스 퍼졌을 때 ans값 구하기
+    int arr[N][N];
+    copy(&tmp[0][0], &tmp[0][0] + N * N, &arr[0][0]);
+    queue<pair<int, int>> q;
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < m; j ++) {
+            if(arr[i][j] == 2) {
+                q.push(make_pair(i,j));
+            }
+        }
+    }
+    while(q.size()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
 
+        for (int i = 0; i < 4; i ++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m)
+                    continue;
+                if(arr[nx][ny] == 0) {
+                    arr[nx][ny] = 2;
+                    q.push(make_pair(nx,ny));
+                }
+                
+        }
+    }
+        int count = 0;
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < m; j ++) {
+                if(arr[i][j] == 0) {
+                    count ++;
+                }
+            }
+        }
+    ans = max(ans, count);
+    
+    
+
+
+}
+
+void wall(int cnt) { // nC3 , 전형적인 재귀함수 구조
+    if(cnt == 3) {
+        bfs();
+        return;
+    }
+
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < m; j ++) {
+            if(tmp[i][j] == 0) {
+                tmp[i][j] = 1;
+                wall(cnt + 1);
+                tmp[i][j] = 0;
+            }
+        }
+    }
 }
 
 int main() {
@@ -32,9 +90,15 @@ int main() {
     for (int i = 0; i < n; i ++) {
         for (int j = 0; j < m; j ++) {
             if(d[i][j] == 0) {
-                
+                memset(visited, 0, sizeof(visited));
+                copy(&d[0][0], &d[0][0] + N * N, &tmp[0][0]);
+                tmp[i][j] = 1;
+                wall(1);
+                tmp[i][j] = 0;
             }
         }
     }
+
+    cout << ans;
 
 }
