5f771e247a546d2c98ac601f08841de8fabda8e9
Jeong Jong Woo <90558704+on-jwo@users.noreply.github.com>
2023-12-24 14:34:22 +0000

    add

--- C++class/10026.cpp
+++ C++class/10026.cpp
@@ -8,24 +8,43 @@ using namespace std;
 
 const int N = 102;
 
-bool visites[N][N];
+bool visited[N][N];
 char d[N][N];
 char und[N][N];
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
+int n, cnt;
 
-void bfs(char tmp[][N]) {
+void bfs(char d[][N]) {
     queue<pair<int, int>> q;
     q.push({0,0});
-
+    visited[0][0] = true;
     while(!q.empty()) {
-        int a;
+        int x = q.front().first;
+        int y = q.front().second;
+        visited[x][y] = true;
+        char color = d[x][y];
+        q.pop();
+
+        for (int i = 0; i < 4; i ++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            char ncolor = d[nx][ny];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n ) {
+                continue;
+            } 
+            if(ncolor == color) {
+                visited[nx][ny] = true;
+                q.push(make_pair(nx,ny));
+            }
+            
+        }
     }
 }
 
 
 int main() {
-    int n; cin >> n;
+    cin >> n;
     for (int i = 0; i < n; i ++) {
         string s; cin >> s;
         for (int j = 0; j < n; j ++) {
@@ -38,8 +57,24 @@ int main() {
             if(und[i][j] == 'G') und[i][j] = 'R'; 
         }
     }
-
-    bfs(d);
-
-
+    int cnt1 = 0;
+    int cnt2 = 0;
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < n; j ++) {
+            if(!visited[i][j]) {
+                cnt1 ++;
+                bfs(d);
+            }
+        }
+    }
+    fill(&visited[0][0], &visited[n-1][n-1], 0);
+        for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < n; j ++) {
+            if(!visited[i][j]) {
+                cnt2 ++;
+                bfs(und);
+            }
+        }
+    }
+    cout << cnt1 << endl << cnt2;
 }
